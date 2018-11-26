a9019e1230ba5fe6eeff4879bde03ec0ef276678
John Bäckstrand <john.backstrand@se.abb.com>
2018-11-26 08:47:32 +0100

    Some clojure stuff

--- clojure/corelogictest/src/corelogictest/core.clj
+++ clojure/corelogictest/src/corelogictest/core.clj
@@ -1,7 +1,7 @@
 (ns corelogictest.core
   (:refer-clojure :exclude [==])
   (:require [clojure.core.logic :refer :all  :exclude [pldb.indexed?]])
-  (:require [clojure.core.logic.pldb :as pldb :refer [db-fact with-db with-dbs]])
+  (:require [clojure.core.logic.pldb :as pldb :refer [with-db with-dbs]])
   (:gen-class))
 
 (def men
@@ -26,6 +26,9 @@
 (def facts
   (pldb/db-fact facts likes 'Mary 'John))
 
+(def facts
+  (pldb/db-fact facts likes 'John 'Bob))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
@@ -34,13 +37,20 @@
   (pldb/db-rel woman x)
   (pldb/db-rel likes p1 p2)
   (pldb/db-rel fun x)
-  (pldb/with-dbs [men fun-people]
+  (with-dbs [men fun-people]
     (run* [q]
       (man q)
       (fun q)))
-  (pldb/with-dbs [men facts]
+  (with-dbs [men facts]
     (run* [q]
-          (likes 'Mary q)))
-  (pldb/with-dbs [men facts] (run* [q] (fresh [x y] (likes x y) (== q [x y]))))
-  (with-dbs [men facts] (run* [q] (fresh [x y] (likes x y) (likes y x) (== q [x y]))))
-  )
+      (likes 'Mary q)))
+  (with-dbs [men facts] (run* [q] (fresh [x y] (likes x y) 
+                                               (== q [x y]))))
+  (with-dbs [men facts] (run* [q] (fresh [x y z] (likes x y) 
+                                               (likes y z)
+                                               (likes z x)
+                                               (== q [x y z]))))
+  (pprint 3)
+)
+
+(-main)
